6d7623287bf2f9760309f5f2a563eb0c3ccfa9cf
Manuel Castellin <manuel@castellinconsulting.com>
2022-09-05 18:41:39 +0200

    fix: add missing module

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     install_requires=[
         "boto3==1.24.66",
         "botocore==1.27.66",
+        "jinja2==3.1.2",
         "jmespath==1.0.1; python_version >= '3.7'",
         "logzero==1.7.0",
+        "markupsafe==2.1.1; python_version >= '3.7'",
         "python-dateutil==2.8.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
         "pyyaml==6.0",
         "s3transfer==0.6.0; python_version >= '3.7'",
